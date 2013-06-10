ea3ef9dd28309456e67e0dc8cd58dc30363fd2bf
Jennifer Rubinovitz <jrubinovitz@gmail.com>
2013-06-10 18:32:31 -0400

    mining cb progress. switching computers

--- apps/crunchbase/parseCompanies.py
+++ apps/crunchbase/parseCompanies.py
@@ -1,22 +1,28 @@
+"""
+Script for mining crunchbase.
+"""
+from crunchbase import Crunchbase
+from models.companyModels import *
+import ast
+import json
 
-from Crunchbase import Crunchbase
 c = Crunchbase(os.getenv("crunchbase_key"))
-#return jsonify(c.listCompanies)
-companies = c.listCompanies()
 
-for company in companies:
-	dump = json.dumps(company)
-	investments = dump['investments'] # dictionary of investments
-	company_name = dump['name']
-	tags = dump['tag_list']
-	store_investments(investments)
-	store_tags(tags)
+companies= c.listCompanies()
 
-
+for company in range(0, len(companies)):
+	print companies[company]
+#	investments = companies[company]['investments'] # dictionary of investments
+#	print investments
+#	company_name = dump['name']
+#	tags = dump['tag_list']
+#	store_investments(investments)
+#	store_tags(tags)
 
 def store_investments(investments):
 	for investment in investments:
-		
+		pass		
 
 def store__tags(tags):
 	for tag in tags:
+		pass
