7bb0e2127d99048cf6eb9a6c4c4affbd4191555c
Jennifer Rubinovitz <jrubinovitz@gmail.com>
2014-01-10 14:42:29 -0500

    twitter model starting

--- apps/twitter/twitter_model.py
+++ apps/twitter/twitter_model.py
@@ -6,15 +6,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('db_url')
 db = SQLAlchemy(app)
 
-twitter_words_association = db.Table('twitter_words_association',
-    db.Column('twitter_id', db.Integer, db.ForeignKey('twitter_credentials.id')),
-    db.Column('word_id', db.Integer, db.ForeignKey('word.id'))
+# words to watch out for
+twitter_keywords = db.Table('twitter_words',
+		db.Column('word_id', db.Integer, db.ForeignKey('word.id')),
+		db.Column('twitter_user_id', db.Integer, db.ForeignKey('Twitter_model.id'))
 )
 
 class Twitter_model:
-	
-	__tablename__ = "twitter_credentials"
-
 	id = db.Column(db.Integer, primary_key=True)
 	token_expiry = db.Column(db.String)
 	access_token = db.Column(db.String)
@@ -26,5 +24,9 @@ class Twitter_model:
 	id_token = db.Column(db.String)
 	token_response = db.Column(db.String)
 	username = db.Column(db.String)
-	words = roles = db.relationship('word', secondary=roles_users,
+	words =  db.relationship('word', secondary=twitter_keywords,
 			backref=db.backref('twitter_credentials')
+
+class Word:
+	id = db.Column(db.Integer, primary_key=True)
+	word = db.Column(db.String(140))
