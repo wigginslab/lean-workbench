af649865e44231fdbe7c0ccde8aae3be5ddcc1e7
jennifer <jrubinovitz@gmail.com>
2014-01-14 15:28:56 -0500

    gunicorn working

--- lean_workbench/manage.py
+++ lean_workbench/manage.py
@@ -4,18 +4,19 @@ from flask.ext import script
 
 import commands
 
+from gunicorn_server import GunicornServer
+
 if __name__ == "__main__":
     from main import app_factory
     import config
     from flask.ext.script import Server, Manager
     import os
 
-
-
     manager = script.Manager(app_factory)
     manager.add_option("-c", "--config", dest="config", required=False, default=config.Dev)
     manager.add_command("test", commands.Test())
     manager.add_command("create_db", commands.CreateDB())
     manager.add_command("drop_db", commands.DropDB())
+    manager.add_command("gunicorn", GunicornServer())
 
     manager.run()
