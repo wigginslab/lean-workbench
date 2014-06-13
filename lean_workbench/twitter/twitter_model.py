c81939fd60c079539ea841766f809606fa583615
jennifer <jrubinovitz@gmail.com>
2014-06-13 18:15:01 -0400

    added more auto followed words on twitter for each company, twitter api endpoint works on our side

--- lean_workbench/twitter/twitter_model.py
+++ lean_workbench/twitter/twitter_model.py
@@ -48,16 +48,18 @@ class Twitter_model(db.Model):
 	# LWB email
 	username = db.Column(db.String)
 	twitter_handle = db.Column(db.String)
+	company_name = db.Column(db.String)
 	words =  db.relationship('Word', secondary=tracked_twitter_words,
-		backref=db.backref('Twitter_model'))
+		backref=db.backref('twitter'))
 
 	def __init__(self, cred_dict):
 		self.oauth_token =  cred_dict.get('oauth_token')
 		self.oauth_token_secret = cred_dict.get('oauth_token_secret')
 		self.username = cred_dict.get('username')
 		self.twitter_handle = cred_dict.get('twitter_handle')
-		company_name = cred_dict.get('company')
-		self.words = [(Word(word=company_name))]
+		self.company_name = cred_dict.get('company')
+		words = [self.company_name.replace(" ",""), self.company_name, self.company_name.lower(), "#"+self.company_name.replace(" ","").lower(), '#'+self.company_name.replace(" ","")]
+		self.words = [Word(word=x) for x in words]
 
 	def as_dict(self):
 		return {c.name: getattr(self, c.name) for c in self.__table__.columns}
