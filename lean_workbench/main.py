96a4378705c3e8e0a81f6629ff4a534c747a8c8e
jennifer <jrubinovitz@gmail.com>
2014-07-19 15:43:50 -0400

    main fixed so results page rendered

--- lean_workbench/main.py
+++ lean_workbench/main.py
@@ -193,6 +193,7 @@ def configure_views(app):
 	@app.route('/export', methods=['POST','GET'])
 	@app.route('/dashboard', methods=['POST', 'GET'])    
 	@app.route('/scale', methods=['POST', 'GET'])    
+        @app.route('/results', methods=['POST', 'GET'])    
         @app.route('/dashboard2', methods=['POST', 'GET'])    
 
 	def dashboard():
