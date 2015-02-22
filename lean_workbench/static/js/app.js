90b74172464dc29a0ee0cfdb6816f0664920397c
jennifer <jrubinovitz@gmail.com>
2015-02-22 17:36:08 -0500

    stop empty wufoo graph from breaking everything

--- lean_workbench/static/js/app.js
+++ lean_workbench/static/js/app.js
@@ -5,18 +5,9 @@ angular.module(
         'http-auth-interceptor', 'ngCookies', 'ngRoute'
     ]
 )
-
 .config(function($httpProvider) {
   $http.defaults.headers.common['Authentication-Token'] = $.cookie('auth_token');
-
 })
-
-/*.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: '/static/partials/public.html', controller: MyCtrl1});
-    $routeProvider.when('/dashboard', {templateUrl: '/static/partials/dashboard.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-}])*/
-
 .run( function run( $http, $cookies ){
     $http.defaults.headers.post['X-CSRFToken'] = csrf_token;
 })
